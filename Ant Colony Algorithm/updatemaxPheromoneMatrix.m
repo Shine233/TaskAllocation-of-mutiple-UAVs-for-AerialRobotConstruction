58425290377b0c25872f2c339666d308b1966d46
shine <shine19960531@hotmail.com>
2019-08-06 01:08:01 +0100

    Change the folder name

--- Ant Colony Algorithm/updatemaxPheromoneMatrix.m
+++
@@ -1,9 +0,0 @@
-function [maxPheromoneMatrix]=updatemaxPheromoneMatrix(pheromoneMatrix,worker_number,task_number)
-
-for i=1:task_number
-    % Find the column index of the maximum value in each row
-    [C,I]=max(pheromoneMatrix(i,:)); 
-    maxPheromoneMatrix(i)=I;  
-end
-
-end
